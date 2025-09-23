e5d4dbada1ba70bb22a2e0399aa885eea01644ec
aikhe <ikeandrie.ro@gmail.com>
2025-09-23 18:25:04 +0800

    feat(array_dump): params pointer + memory allocation + array dumping

---
+++ miscs/array-dump/array_dump.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// declare parameters of pointers, asteris so it can read and reflect changes to
+// passed arguments from main function
+void append_int(int **arr, int *size, int *capacity, int value) {
+  if (*size >= *capacity) {
+
+    // doubles capacity if 0
+    *capacity = (*capacity == 0) ? 1 : (*capacity * 2);
+    int *new_arr = realloc(*arr, (*capacity) * sizeof(int));
+
+    // if (!new_arr) {
+    //   fprintf(stderr, "Memory allocation failed\n");
+    //   exit(EXIT_FAILURE);
+    // }
+
+    // apply new changes to arr
+    *arr = new_arr;
+  }
+
+  // this is where the magic happen, appends each value
+  (*arr)[*size] = value;
+  (*size)++;
+}
+
+int main() {
+  int *arr = NULL;
+  int size = 0;
+  int capacity = 0;
+
+  for (int i = 0; i < 5; ++i) {
+    append_int(&arr, &size, &capacity, i);
+  }
+
+  // print arr
+  for (int i = 0; i < size; ++i) {
+    printf("%d \n", arr[i]);
+  }
+
+  // deallocate dynamic memory (prevents memory leak), used with realloc() &
+  // malloc()
+  free(arr);
+
+  return 0;
+}
